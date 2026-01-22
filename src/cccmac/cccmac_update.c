e4ab50d57d9407ec1b29d8106a648faa9c4dd110
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-01-23 09:03:36 +1100

    cc(cmac): light change in function calls

--- src/cccmac/cccmac_update.c
+++ src/cccmac/cccmac_update.c
@@ -19,6 +19,7 @@
 #include <corecrypto/cc_priv.h>
 #include <corecrypto/cccmac.h>
 #include <corecrypto/cccmac_priv.h>
+#include <corecrypto/ccmode.h>
 
 int cccmac_update(cccmac_ctx_t ctx, size_t data_nbytes, const void *data)
 {
@@ -30,7 +31,7 @@ int cccmac_update(cccmac_ctx_t ctx, size_t data_nbytes, const void *data)
     cc_memcpy(cccmac_block(ctx), data, CMAC_BLOCKSIZE);
 
     while (nblocks--) {
-        cccmac_cbc(ctx)->cbc(cccmac_mode_sym_ctx(cccmac_cbc(ctx), ctx), cccmac_mode_iv(cccmac_cbc(ctx), ctx), 1, cccmac_block(ctx), cccmac_block(ctx));
+        cccbc_update(cccmac_cbc(ctx), cccmac_mode_sym_ctx(cccmac_cbc(ctx), ctx), cccmac_mode_iv(cccmac_cbc(ctx), ctx), 1, cccmac_block(ctx), cccmac_block(ctx));
     }
 
     cc_try_abort("ZORMEISTER: incomplete CMAC function called. raising hell.\n");
