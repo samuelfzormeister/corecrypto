e3b0903a207be2c25bb276acf563fb92d8ce43fe
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-02-10 21:46:46 +1100

    cc(uptime): Implement cc_uptime_seconds for Windows.
    Signed-off-by: Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>

---
+++ src/cc/cc_uptime.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2026 The PureDarwin Project, All rights reserved.
+ *
+ * @LICENSE_HEADER_BEGIN@
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @LICENSE_HEADER_END@
+ */
+
+#include <corecrypto/cc_uptime.h>
+
+#if CC_WINDOWS
+
+#include <sysinfoapi.h>
+
+uint32_t cc_uptime_seconds() {
+    uint32_t ticks = GetTickCount();
+    return (ticks / 1000);
+};
+
+#endif
