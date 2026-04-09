87e14ea5b064dd6dabf41c5dedd13d55677fc3f8
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-04-09 14:46:59 +1000

    ccpbkdf2: Rework implementation.
    Signed-off-by: Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>

--- src/ccpbkdf2/ccpbkdf2_hmac.c
+++ src/ccpbkdf2/ccpbkdf2_hmac.c
@@ -20,24 +20,11 @@
 #include <corecrypto/cc_priv.h>
 #include <corecrypto/cchmac.h>
 #include <corecrypto/ccpbkdf2.h>
+#include <corecrypto/ccdigest_priv.h>
 
-/*
- * read this for more info:
- * https://www.rfc-editor.org/rfc/rfc2898
- */
 
-void PRF(const struct ccdigest_info *di, size_t passwordLen, const void *password,
-         size_t length, const void *data,
-         void *output)
-{
-    cchmac(di, passwordLen, password, length, data, output);
-}
-
-/* the base size of a workspace, though i'll change it to be dynamic. */
-#define CCPKDDF2_WORKSPACE_BUFFER_SIZE_N(hlen) ccn_nof_size(hlen) + ccn_nof_size(4)
-
-/* i wonder if i should run this all in the stack as opposed to allocating small pockets of memory */
-#define CCPBKDF2_WORKSPACE_SALT_SIZE_N(saltLen) ccn_nof_size(saltLen) + ccn_nof_size(sizeof(uint32_t) * 2)
+// --- https://www.rfc-editor.org/rfc/rfc2898 --- //
+// --- https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-132.pdf --- //
 
 int ccpbkdf2_hmac(const struct ccdigest_info *di,
                   size_t passwordLen, const void *password,
@@ -46,46 +33,64 @@ int ccpbkdf2_hmac(const struct ccdigest_info *di,
                   size_t dkLen, void *dk)
 {
     size_t hLen = di->output_size;
-    CC_WORKSPACE_STACK_DECL_N(counter, CCPBKDF2_WORKSPACE_SALT_SIZE_N(saltLen)); /* Salt/Counter buffer */
-    CC_WORKSPACE_STACK_DECL_N(buffer1, CCPKDDF2_WORKSPACE_BUFFER_SIZE_N(hLen));
-    CC_WORKSPACE_STACK_DECL_N(buffer2, CCPKDDF2_WORKSPACE_BUFFER_SIZE_N(hLen));
-    CC_WORKSPACE_STACK_DECL_N(blockbuf, CCPKDDF2_WORKSPACE_BUFFER_SIZE_N(hLen));
-    uint8_t *salt_ptr = (uint8_t *)counter->start;
-    size_t nblocks = (dkLen + hLen - 1) / hLen;
-    size_t remainder = dkLen % hLen;
-    remainder = remainder ? remainder : hLen;
-    size_t counterlen = saltLen + 4;
+    cchmac_di_decl(di, hmac);
+    CC_WORKSPACE_STACK_DECL(state, di->state_size); // --- Use CC_WORKSPACE to enable Windows clients. --- //
+    cc_unit *istate = state->start;
 
-    /* copy the salt into the counter */
-    cc_memcpy(counter->start, salt, saltLen);
-
-    for (int cnt = 0; cnt < nblocks; cnt++) {
-        /* compute U_1 */
-        int block = cnt + 1;
-        size_t outLen = block != nblocks ? hLen : remainder;
-
-        /* encode the counter */
-        for (int i = 0; i < 4; i++) {
-            salt_ptr[saltLen + i] = (block >> 8 * (3 - i)) & 0xff;
-        }
-
-        /* run the PRF for U_1 */
-        PRF(di, passwordLen, password, counterlen, salt, buffer1->start);
-
-        cc_memcpy(blockbuf->start, buffer1->start, counterlen);
-
-        for (int iter = 1; iter < iterations; iter++) {
-            PRF(di, passwordLen, password, hLen, buffer1->start, buffer2->start);
-            cc_memcpy(buffer1->start, buffer2->start, hLen);
-            cc_xor(hLen, blockbuf->start, blockbuf->start, buffer1->start);
-        }
-
-        cc_memcpy(dk + (block * hLen), blockbuf->start, outLen);
+    // --- This is to match SP800-132's PBKDF2 spec. --- //
+    if ((dkLen / di->output_size) > UINT32_MAX) {
+        return CCERR_PARAMETER;
     }
 
-    CC_WORKSPACE_STACK_FREE_N(counter, CCPBKDF2_WORKSPACE_SALT_SIZE_N(saltLen));
-    CC_WORKSPACE_STACK_FREE_N(buffer1, CCPKDDF2_WORKSPACE_BUFFER_SIZE_N(hLen));
-    CC_WORKSPACE_STACK_FREE_N(buffer2, CCPKDDF2_WORKSPACE_BUFFER_SIZE_N(hLen));
-    CC_WORKSPACE_STACK_FREE_N(blockbuf, CCPKDDF2_WORKSPACE_BUFFER_SIZE_N(hLen));
+    //
+    // every invocation to HMAC uses the password as the key.
+    //
+    // thus, initialise a HMAC context and save the state to reduce
+    // unnecessary computation.
+    //
+    cchmac_init(di, hmac, passwordLen, password);
+    ccdigest_copy_state(di, istate, cchmac_istate(di, hmac));
+
+    //
+    // cchmac_final runs ccdigest_final on the internal cchmac_data
+    // prior to finalising the MAC, which means we can use cchmac_data
+    // to store our last MAC, which will then get compressed to generate
+    // the new MAC.
+    //
+    for (uint32_t block = 1; dkLen > 0; block++) {
+        uint32_t counter;
+        size_t outLen = CC_MIN(dkLen, hLen);
+        uint8_t *buffer = cchmac_data(di, hmac);
+
+        CC_STORE32_BE(block, &counter);
+
+        // --- Ensure that this is a fresh state. --- //
+        ccdigest_copy_state(di, cchmac_istate(di, hmac), istate);
+        cchmac_nbits(di, hmac) = di->block_size * 8;
+        cchmac_num(di, hmac) = 0;
+
+        cchmac_update(di, hmac, saltLen, salt);
+        cchmac_update(di, hmac, sizeof(uint32_t), &counter);
+
+        cchmac_final(di, hmac, buffer);
+
+        cc_copy(outLen, dk, buffer);
+
+        for (size_t i = 2; i <= iterations; i++) {
+            // --- This resets the HMAC state to one where we can generate the new MAC. --- //
+            ccdigest_copy_state(di, cchmac_istate(di, hmac), istate);
+            cchmac_nbits(di, hmac) = di->block_size * 8;
+            cchmac_num(di, hmac) = di->output_size;
+            cchmac_final(di, hmac, buffer);
+            cc_xor(outLen, dk, dk, buffer);
+        }
+
+        dk += outLen;
+        dkLen -= outLen;
+        outLen = CC_MIN(dkLen, hLen);
+    }
+
+    CC_WORKSPACE_STACK_FREE(state, di->state_size);
+
     return CCERR_OK;
 }
